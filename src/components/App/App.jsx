c256835923027dae272e4f72e2a7ba462ffb45a3
Viktor Biruk <vikttur.job@gmail.com>
2023-06-17 00:31:07 +0300

    creates modal and button components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,50 @@
-import css from './App.module.css';
-// import Button from '../Button/Button';
+import React, { Component } from 'react';
+// import css from './App.module.css';
+import Button from '../Button/Button';
 // import ImageGallery from '../ImageGallery/ImageGallery';
 // import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 // import Loader from '../Loader/Loader';
-// import Modal from '../Modal/Modal';
+import Modal from '../Modal/Modal';
 // import Searchbar from '../Searchbar/Searchbar';
 // import SearchForm from '../SearchForm/SearchForm';
+import { ReactComponent as AddIcon } from '../../icons/add.svg';
 
-export default function App() {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-      className={css.App}
-    >
-      React homework template
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  componentDidMount() {
+    // console.log('Modal componentDidMount');
+  }
+
+  componentWillUnmount() {
+    // console.log('Modal WillUnmount');
+  }
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
+  };
+
+  render() {
+    const { showModal } = this.state;
+
+    return (
+      <>
+        <Button onClick={this.toggleModal}>
+          <AddIcon width="80" height="20" fill="blue" />
+        </Button>
+
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <h1>Заголовок</h1>
+
+            <button type="button" onClick={this.toggleModal}>
+              Close modal
+            </button>
+          </Modal>
+        )}
+      </>
+    );
+  }
 }
